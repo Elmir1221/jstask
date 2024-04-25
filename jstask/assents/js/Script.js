c956565c0630f25e53152d346a68b01b8ecf4b72
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-04-25 08:29:50 +0400

    Add files via upload

---
+++ jstask/assents/js/Script.js
@@ -0,0 +1,190 @@
+"use Strict"
+
+// console.log("sdsad");
+
+// var adrtess="yghilk";
+// adress=55;
+// console.log(adrtess)
+// let adrtess="tfghjbnm";
+
+// var isMarried = true;
+
+// const email="asdasd@fdsasd.com";
+// let phone="541321653";
+
+// console.log(100+100)
+
+// let a= 5;
+// let b=5;
+// let result=a+b;
+// console.log(result);
+// a++;
+// console.log(a);
+
+// let isMarried=true;
+// let isnumber = 200;
+// if( isMarried=true)
+// {
+//     console.log("evlidir");
+// }
+// else
+// {
+//     console.log("subay");
+
+// }
+
+
+// if(number > 50 && number < 150)
+// {
+//     console.log(yes)
+// }
+// else
+// {
+//     console.log(no)
+// }
+
+// let weekday= "wednesday"
+// switch(weekday)
+// {
+//     case"tuesday":
+//     console.log("tuesday")
+//     case"wednesday":
+//     console.log("wednesday")
+//        break;
+//     default:
+//         console.log("not selected")
+//         break;
+// }
+
+// alert("welcome class")
+
+// let result = confirm("press a button");
+// console.log(result)
+
+// if(person==null)
+// {
+//     console.log("operation canceled")
+// }
+// else
+// {
+// console.log(person)
+// }
+
+// let a= 5;
+// while(a<8)
+// {
+// console.log(a)
+//     a++; 
+// }
+
+// for (let i = 0; i < 50; i++) 
+// {
+//     console.log(i);
+// }
+
+// let a= 5;
+// let b =a**2;
+// console.log()
+
+// let num1 =200;
+// let num2 = "100";
+// console.log(num1==num2)
+
+//let datas = [1,2,3, "salam", true];
+// console.log(datas[3])
+
+// for (let i = 0; i < datas.length; i++) 
+// {
+//     console.log(datas[i]); 
+// }
+
+// for (let i = 0; i < datas.length; i++) 
+// {
+//     if(isNaN(datas[i])) 
+//     {
+//     console.log(datas[i])
+    
+//     }
+
+// }
+
+
+// for (let i = 0; i < datas.length; i++) 
+// {
+//     if(!isNaN(datas[i])) 
+//     {
+//     console.log(datas[i])
+//     }
+// }
+
+// let isActive= true;
+// console.log(typeof isActive=='boolean')
+
+// for (let i = 0; i < datas.length; i++) 
+// {
+//     if(!isNaN(datas[i]) && typeof datas[i] !='bolean') 
+//     {
+//     console.log(datas[i])
+//     }
+// }
+
+// for (let i = 0; i < datas.length; i++) 
+// {
+//     if(typeof datas[i]=='number') 
+//     {
+//     console.log(datas[i])
+//     }
+// }
+
+// let elan =50;
+// let result=elem>100 ? "yes": "no";
+// console.log(result);
+
+// let a =null;
+// let b;
+// console.log(a)
+// console.log(b)
+// console.log(a+b);
+
+// let number = [36,6,8,9,15,11];
+// let sum=0;
+// for (let i = 0; i < number.length; i++) {
+//     sum+=number[i];
+    
+// }
+// console.log(sum);
+
+// let isTrue =true;
+// if (isTrue) {
+//   //var name ="fexriyye"
+//     let name ="fexriyye"
+
+// }
+
+// console.log(name)
+
+// if (isTrue) {
+//     let adress= "sadhasd";
+//     if(true){
+//         let surname="jjj"
+//     }
+  
+//   }
+//   console.log(surname);
+
+//const data = "product1"
+// const product =["hgk","jhj"];
+
+// for (let i = 0; i < product.length; i++) {
+//     console.log(product[i]);
+// }
+
+// const name="test2";
+// name= "test 1";
+
+// const datas =[1,2,3,4,5];
+// const datas2 =[1,2,3,4,5];
+// datas=datas2
+// datas[2] =100;
+
+
